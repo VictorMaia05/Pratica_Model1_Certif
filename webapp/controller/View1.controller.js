6ca1ff181ad2d1798c913976bab83f0894bea3d0
VictorMaia05 <victormaia0505@gmail.com>
2024-11-08 23:24:55 +0000

    Validações extras em Dialog

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -2,10 +2,10 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/ui/core/Fragment",
-    "mentoria/fiori/ka/model1/vsm/zkauimodel1vsm/Util/Formatter"
-
+    "mentoria/fiori/ka/model1/vsm/zkauimodel1vsm/Util/Formatter",
+    "sap/m/MessageBox"
 ],
-function (Controller, JSONModel, Fragment, Formatter) {
+function (Controller, JSONModel, Fragment, Formatter, MessageBox) {
     "use strict";
 
     return Controller.extend("mentoria.fiori.ka.model1.vsm.zkauimodel1vsm.controller.View1", {
@@ -16,6 +16,33 @@ function (Controller, JSONModel, Fragment, Formatter) {
             let oModelJson = new JSONModel();
             oModelJson.loadData("model/TableModel.json")
             this.getView().setModel(oModelJson);
+        },
+
+        check_inputs: function () {
+            let sError = false;
+
+            let sNome = this.getView().byId("inputNome").getValue();
+            if (sNome.length <= 0) {
+                sError = true; 
+            };
+
+            let sData = this.getView().byId("inputDt").getValue();
+            if (sData.length <= 0) {
+                sError = true; 
+            }
+            else if (this.getView().byId("inputDt").isValidValue() == false) {
+                sError = true; 
+            };
+
+            let sAltura = this.getView().byId("inputAltura").getValue();
+            if (sAltura == 0.00) {
+                sError = true; 
+            }
+            //else if (isNaN(sAltura) && isNaN(parseFloat(sAltura))) {
+            //    sError = true; 
+            //}
+
+            return sError;
         },
 
         onClickNewCustomer: function(oEvent) {
@@ -42,35 +69,42 @@ function (Controller, JSONModel, Fragment, Formatter) {
 
         onSubmitCadastro: function(oEvent) {
 
-            let oModelForm = this.getView().getModel().getProperty("/dataForm");
+            let sHasError = this.check_inputs();
 
-            let oModelTable = this.getView().getModel().getProperty("/dataTable");
+            if (sHasError == false) {
 
-            // Se não seleciono nenhum, ele não consegue entender o FEMININO.
-            // Ao colocar o código abaixo não sei pq ele carrega no MODEL. \\
+                let oModelForm = this.getView().getModel().getProperty("/dataForm");
 
-            let sSexo = this.getView().byId("selectedSexo").getSelectedKey(); 
-
-            oModelTable.push(oModelForm);
-
-            oEvent.getSource().getParent().destroy();
-
-            this.getView().getModel().setProperty("/dataTable", oModelTable);
-
-            // Criei esse objeto pois ao mudar o model direto estava alterando também o model da tabela. \\
-            let oModelFormNew = Object.create(oModelForm);
-
-            for (const property in oModelFormNew) {
-                oModelFormNew[property] = "";
-            };
-
-            this.getView().getModel().setProperty("/dataForm", oModelFormNew);
-
-            // Sei que não necessitamos desse código,
-            // pois já setamos o SETPROPERTY do MODEL,
-            // Mas por algum motivo algumas vezes notei que o dado está no MODEL mas não atualiza a tabela. \\
-            this.byId("customerTable").getBinding("items").refresh();
-
+                let oModelTable = this.getView().getModel().getProperty("/dataTable");
+    
+                // Se não seleciono nenhum, ele não consegue entender o FEMININO.
+                // Ao colocar o código abaixo não sei pq ele carrega no MODEL. \\
+    
+                let sSexo = this.getView().byId("selectedSexo").getSelectedKey(); 
+    
+                oModelTable.push(oModelForm);
+    
+                oEvent.getSource().getParent().destroy();
+    
+                this.getView().getModel().setProperty("/dataTable", oModelTable);
+    
+                // Criei esse objeto pois ao mudar o model direto estava alterando também o model da tabela. \\
+                let oModelFormNew = Object.create(oModelForm);
+    
+                for (const property in oModelFormNew) {
+                    oModelFormNew[property] = "";
+                };
+    
+                this.getView().getModel().setProperty("/dataForm", oModelFormNew);
+    
+                // Sei que não necessitamos desse código,
+                // pois já setamos o SETPROPERTY do MODEL,
+                // Mas por algum motivo algumas vezes notei que o dado está no MODEL mas não atualiza a tabela. \\
+                this.byId("customerTable").getBinding("items").refresh();
+            }
+            else {
+                MessageBox.error("Preencher campos obrigatórios no padrão correto!");
+            }
         },
 
         onCloseCadastro: function(oEvent) {
